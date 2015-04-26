1de9f6ab898174c4ffa90f5d6c0e194fc6c8af94
alejandrodob <alejandrodob@gmail.com>
2015-04-26 20:06:09 +0200

    Retrieve director and movies names

---
+++ javipelis.py
@@ -0,0 +1,38 @@
+import os
+
+class Finder(object):
+
+    def __init__(self):
+        self._directors = []
+        self._movies = []
+
+    def discover(self, path):
+        files_in_path = os.listdir(path)
+        self._directors = self._extract_directors(files_in_path)
+        self._movies = self._extract_movies(files_in_path)
+
+    def _extract_directors(self, filenames_list):
+        return [self._get_director_name(filename).strip()
+                for filename in filenames_list
+                if self._get_director_name(filename)]
+
+    def _get_director_name(self, filename):
+        if len(filename.split('.')) > 1:
+            return filename.split('.')[0]
+
+    def _extract_movies(self, filenames_list):
+        return [self._get_movie_title(filename).strip()
+                for filename in filenames_list
+                if self._get_movie_title(filename)]
+
+    def _get_movie_title(self, filename):
+        if len(filename.split('.')) > 1:
+            return filename.split('.')[1]
+
+    @property
+    def directors(self):
+        return self._directors
+
+    @property
+    def movies(self):
+        return self._movies
--- spec/core_spec.py
+++ spec/core_spec.py
@@ -1,3 +1,31 @@
-with description('javipelis'):
-    with it('does stuff'):
-        assert True is True
+import os
+import sys
+topdir = os.path.join(os.path.dirname(__file__), "..")
+sys.path.append(topdir)
+import javipelis
+
+with description('movie finder'):
+
+    with context('discovering folders and files'):
+
+        with before.all:
+            self.test_dir = 'spec/movie-test-dir'
+
+        with before.each:
+            self.finder = javipelis.Finder()
+
+        with it('creates entries for found directors'):
+            self.finder.discover(self.test_dir)
+
+            assert 'Kubrik, Stanley' in self.finder.directors, self.finder.directors
+
+        with it('does not create noisy entries for random files found'):
+            self.finder.discover(self.test_dir)
+
+            assert '' not in self.finder.directors, self.finder.directors
+            assert None not in self.finder.directors, self.finder.directors
+
+        with it('stores the title of the movies found for each director'):
+            self.finder.discover(self.test_dir)
+
+            assert 'Alien' in self.finder.movies, self.finder.movies
