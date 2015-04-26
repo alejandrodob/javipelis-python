67e1b3b5f5cac38f89c5d19a02a3d3bcfc0d17e8
alejandrodob <alejandrodob@gmail.com>
2015-04-26 20:43:04 +0200

    Possible hidden file detection for Windows (needs real testing)

--- javipelis.py
+++ javipelis.py
@@ -1,5 +1,15 @@
 import os
 
+if os.name == 'nt':
+    import win32api, win32con
+
+
+def is_hidden(_file):
+    if os.name == 'nt':
+        attribute = win32api.GetFileAttributes(_file)
+        return attribute & (win32con.FILE_ATTRIBUTE_HIDDEN | win32con.FILE_ATTRIBUTE_SYSTEM)
+    else:
+        return _file.startswith('.')
 
 class Finder(object):
 
@@ -8,7 +18,7 @@ class Finder(object):
         self._movies = []
 
     def discover(self, path):
-        files_in_path = [f for f in os.listdir(path) if not f.startswith('.')]
+        files_in_path = [f for f in os.listdir(path) if not is_hidden(f)]
         self._directors = self._extract_directors(files_in_path)
         self._movies = self._extract_movies(files_in_path)
 
